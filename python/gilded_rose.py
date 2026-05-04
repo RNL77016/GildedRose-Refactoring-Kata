b22c5d3ce2442d47588cc929c522f70406e36ebe
RNL77016 <rnl77016@lasallebajio.edu.mx>
2026-05-03 20:18:41 -0600

    B2: Extract _update_aged_brie and _update_backstage_pass methods

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -25,33 +25,39 @@ class GildedRose(object):
                 if not self._is_sulfuras(item):
                     item.quality = item.quality - 1
 
-    def update_quality(self):
-        for item in self.items:
-            if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
-                self._update_normal_item(item)
-            else:
+    def _update_aged_brie(self, item):
+        """Updates quality and sell_in for Aged Brie."""
+        if item.quality < 50:
+            item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.quality < 50:
+                item.quality = item.quality + 1
+
+    def _update_backstage_pass(self, item):
+        """Updates quality and sell_in for Backstage passes."""
+        if item.quality < 50:
+            item.quality = item.quality + 1
+            if item.sell_in < 11:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == BACKSTAGE_PASSES:
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                if not self._is_sulfuras(item):
-                    item.sell_in = item.sell_in - 1
-                if item.sell_in < 0:
-                    if item.name != AGED_BRIE:
-                        if item.name != BACKSTAGE_PASSES:
-                            if item.quality > 0:
-                                if not self._is_sulfuras(item):
-                                    item.quality = item.quality - 1
-                        else:
-                            item.quality = item.quality - item.quality
-                    else:
-                        if item.quality < 50:
-                            item.quality = item.quality + 1
+            if item.sell_in < 6:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+        item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            item.quality = 0
+
+    def update_quality(self):
+        for item in self.items:
+            if item.name == AGED_BRIE:
+                self._update_aged_brie(item)
+            elif item.name == BACKSTAGE_PASSES:
+                self._update_backstage_pass(item)
+            elif self._is_sulfuras(item):
+                pass  # Sulfuras never changes
+            else:
+                self._update_normal_item(item)
 
 
 class Item:
