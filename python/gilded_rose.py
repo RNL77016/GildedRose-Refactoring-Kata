f4f7c2071908205116e236a93bc0e34c45f36264
RNL77016 <rnl77016@lasallebajio.edu.mx>
2026-05-03 20:18:01 -0600

    B2: Extract _update_normal_item and _is_sulfuras methods

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -10,12 +10,25 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    def _is_sulfuras(self, item):
+        return item.name == SULFURAS
+
+    def _update_normal_item(self, item):
+        """Updates quality and sell_in for normal items."""
+        if item.quality > 0:
+            if not self._is_sulfuras(item):
+                item.quality = item.quality - 1
+        if not self._is_sulfuras(item):
+            item.sell_in = item.sell_in - 1
+        if item.sell_in < 0:
+            if item.quality > 0:
+                if not self._is_sulfuras(item):
+                    item.quality = item.quality - 1
+
     def update_quality(self):
         for item in self.items:
             if item.name != AGED_BRIE and item.name != BACKSTAGE_PASSES:
-                if item.quality > 0:
-                    if item.name != SULFURAS:
-                        item.quality = item.quality - 1
+                self._update_normal_item(item)
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -26,19 +39,19 @@ class GildedRose(object):
                         if item.sell_in < 6:
                             if item.quality < 50:
                                 item.quality = item.quality + 1
-            if item.name != SULFURAS:
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != AGED_BRIE:
-                    if item.name != BACKSTAGE_PASSES:
-                        if item.quality > 0:
-                            if item.name != SULFURAS:
-                                item.quality = item.quality - 1
+                if not self._is_sulfuras(item):
+                    item.sell_in = item.sell_in - 1
+                if item.sell_in < 0:
+                    if item.name != AGED_BRIE:
+                        if item.name != BACKSTAGE_PASSES:
+                            if item.quality > 0:
+                                if not self._is_sulfuras(item):
+                                    item.quality = item.quality - 1
+                        else:
+                            item.quality = item.quality - item.quality
                     else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
 
 
 class Item:
